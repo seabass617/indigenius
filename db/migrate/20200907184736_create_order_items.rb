95db703b5bc0b2d7dabe50005a5cf7dc76a7f534
Cássia Aveliz <62448073+cassiaveliz@users.noreply.github.com>
2020-09-07 16:07:51 -0300

    Update 20200907184736_create_order_items.rb
    fixing typo

--- db/migrate/20200907184736_create_order_items.rb
+++ db/migrate/20200907184736_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration[6.0]
   def change
     create_table :order_items do |t|
-      t.references :workshoDates, null: false, foreign_key: true
+      t.references :workshopDates, null: false, foreign_key: true
       t.references :items, null: false, foreign_key: true
       t.integer :quantity
       t.float :price
