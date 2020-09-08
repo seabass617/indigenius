8619e6f4281d28362068a43acf2528de96654fdb
Saraah Bikai <supaerv@gmail.com>
2020-09-08 14:34:21 -0100

    Added the total price method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,17 @@ class OrdersController < ApplicationController
         @orders = Order.where(user: current_user)
     end   
 
-#to do:implemnent status method  purchased, pending and cancelled
-#to do: implement price calculation
+#to do:implemnent status method  purchased, pending and cancelled// implement that the user can cancel if needed
+# if user cancels  set status to cancelled. 
+# if user completed set status to completed: validation
+# if order pending set status to pending 
+#discuss with team  method of how it would work 
+
+
+    def total_price
+       items.to_a.sum { |item| item.total_price }
+    end
+
     private 
     def order_params 
     params.require(:order).permit(:status, :total_price)
