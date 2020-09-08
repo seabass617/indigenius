cbe4365c35007e2d3fc18df9cffeb3bd38ade0be
Saraah Bikai <supaerv@gmail.com>
2020-09-08 20:42:57 -0100

    Add a destroy method that I will destroy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,45 @@ class OrdersController < ApplicationController
 # if order pending set status to pending 
 #discuss with team  method of how it would work 
 
+#pending by default 
+#completed  when the buyer presses the purchase button 
+#cancelled when cancelled from the buyer's or the seller's side.
+
+# completed and purchased button will be in the order items controller 
+
+
+
+def update 
+    #only the seller can update in case of cancellation 
+    #for both items
+    #reimburse the money 
+    #update the status of the order to cancelled 
+    #update for pending and cancel is the same thing 
+end 
+
+
+def destroy
+    @order_items= OrderItem.find(params[:order_items]) 
+    @order = Order.new(order_params)
+    @order = @order_items
+    @order_items = @order.item_id
+    @order.item_id.destroy
+end
+
+# #def status
+#  # if  @order.current_user
+#    #@order.status = 'completed'
+# # 
+#    elsif destroy
+#     @order.status = 'cancelled'
+  
+#     else pending
+ 
+#         @order.status = 'pending'
+#  #clicks on btn  
+    
+# end     
+
 
     def total_price
        items.to_a.sum { |item| item.total_price }
