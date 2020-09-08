62438654dea19a31514307087c31382b30f42f55
bruno talarico <bruno.talarico@gmail.com>
2020-09-08 11:06:18 -0300

    Items CRUD working, not styled nor showing lots of info.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -63,13 +63,14 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.require(:item).permit(:name, :description, :user_id, :price, :category, :capacity, :quantity, :workshop)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def item_params
+    params.require(:item).permit(:name, :description, :user_id, :price, :category, :capacity, :quantity, :workshop)
+  end
 end
