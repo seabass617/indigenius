6fb13d7ae8bd0f237c80786f5b45884c748ffcd3
Cassia Aveliz <cassia.avelizr@gmail.com>
2020-09-08 12:55:40 -0300

    Order_items CRUD

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,14 +2,22 @@ class OrderItemsController < ApplicationController
   before_action set_order_item, only: %i[edit update destroy]
 
   def index
+    @current_order_items = OrderItem.where(order_id: current_user.order.id) #review this!
   end
 
   def new
     @item = Item.find(params[:item_id])
+    @order_item = OrderItem.new
   end
 
   def create
+    @order = current_user.order.find_or_create_by(status: 'open') #check what will be the options od status
+    @item = Item.find(params[:item_id])
+
     @order_item = OrderItem.new(order_item_params)
+    @order_item.order_id = @order.id
+    @order_item.price = @item.price
+    @order_item.save
   end
 
   def edit
@@ -21,6 +29,7 @@ class OrderItemsController < ApplicationController
   def destroy
     @order_item.destroy
   end
+
   private
 
   def set_order_item
