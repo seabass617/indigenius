3a8164b466d10e4b22ba3f3dac5e4007689e7ae3
bruno talarico <bruno.talarico@gmail.com>
2020-09-08 17:18:50 -0300

    some changes on show page for items, created delete route to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items
-  resources :orders, :only [:index]
+  resources :items, except: :destroy
+  delete '/items/:id', to: 'items#destroy', as: 'delete_item'
+
+  #resources :orders, :only [:index]
   get '/myorders', to: 'orders#index'
 end
