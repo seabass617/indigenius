fb5fe1a3b8244a854227687b80748f6bd4a710be
Cassia Aveliz <cassia.avelizr@gmail.com>
2020-09-09 15:18:56 -0300

    Add products seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,84 +2,137 @@ require 'open-uri'
 require 'nokogiri'
 require 'faker'
 
-p 'Destroying all order...'
-Order.destroy_all
-p 'Destroying all reviews...'
-Review.destroy_all
-p 'Destroying all items...'
-Item.destroy_all
-p 'Destroying all users...'
-User.destroy_all
+# p 'Destroying all order...'
+# Order.destroy_all
+# p 'Destroying all reviews...'
+# Review.destroy_all
+# p 'Destroying all items...'
+# Item.destroy_all
+# p 'Destroying all users...'
+# User.destroy_all
 
-p ''
+# p ''
 
-p 'Creating 10 new Users...'
+# p 'Creating 10 new Users...'
 
-10.times do
-  user = User.new
-  user.first_name = Faker::Name.first_name
-  user.last_name = Faker::Name.last_name
-  user.email = "#{user.first_name}_#{user.last_name}@gmail.com"
-  user.password = '123456'
-  user.password_confirmation = '123456'
-  user.save!
-  user.avatar.attach(io: open('https://source.unsplash.com/featured/?indigenous-people'),
-                     filename: "#{user.first_name}_avatar.jpg")
+# 10.times do
+#   user = User.new
+#   user.first_name = Faker::Name.first_name
+#   user.last_name = Faker::Name.last_name
+#   user.email = "#{user.first_name}_#{user.last_name}@gmail.com"
+#   user.password = '123456'
+#   user.password_confirmation = '123456'
+#   user.save!
+#   user.avatar.attach(io: open('https://source.unsplash.com/featured/?indigenous-people'),
+#                      filename: "#{user.first_name}_avatar.jpg")
 
-  p "#{user.first_name} created with email #{user.email}"
-end
+#   p "#{user.first_name} created with email #{user.email}"
+# end
 
-p ''
+# p ''
 
-p 'Creating new worskshops...'
+# p 'Creating new worskshops...'
 
-craft_url = 'https://www.viator.com/en-CA/Marrakech-tours/Craft-Classes/d5408-g26051-c33939'
-craft = Nokogiri::HTML(open(craft_url).read)
-p 'Looking for craft worskshops...'
+# craft_url = 'https://www.viator.com/en-CA/Marrakech-tours/Craft-Classes/d5408-g26051-c33939'
+# craft = Nokogiri::HTML(open(craft_url).read)
+# p 'Looking for craft worskshops...'
 
-cooking_url = 'https://www.viator.com/en-CA/Marrakech-tours/Cooking-Classes/d5408-g6-c19'
-cooking = Nokogiri::HTML(open(cooking_url).read)
-p 'Looking for cooking worskshops...'
+# cooking_url = 'https://www.viator.com/en-CA/Marrakech-tours/Cooking-Classes/d5408-g6-c19'
+# cooking = Nokogiri::HTML(open(cooking_url).read)
+# p 'Looking for cooking worskshops...'
 
-painting_url = 'https://www.viator.com/en-CA/India-tours/Painting-Classes/d723-g26051-c32009'
-painting = Nokogiri::HTML(open(painting_url).read)
-p 'Looking for painting worskshops...'
+# painting_url = 'https://www.viator.com/en-CA/India-tours/Painting-Classes/d723-g26051-c32009'
+# painting = Nokogiri::HTML(open(painting_url).read)
+# p 'Looking for painting worskshops...'
 
-pottery_url = 'https://www.viator.com/en-CA/India-tours/Pottery-Classes/d723-g26051-c32012'
-pottery = Nokogiri::HTML(open(pottery_url).read)
-p 'Looking for pottery worskshops...'
+# pottery_url = 'https://www.viator.com/en-CA/India-tours/Pottery-Classes/d723-g26051-c32012'
+# pottery = Nokogiri::HTML(open(pottery_url).read)
+# p 'Looking for pottery worskshops...'
 
-p ''
+# p ''
 
-[craft, cooking, painting, pottery].each do |workshop|
-  i = 0
-  until i > 5
-    # scraping info
-    name = workshop.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
-    description = workshop.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
-    price = workshop.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
-    img = workshop.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
-    img.attribute('data-src').value
+# [craft, cooking, painting, pottery].each do |workshop|
+#   i = 0
+#   until i > 5
+#     # scraping info
+#     name = workshop.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
+#     description = workshop.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
+#     price = workshop.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
+#     img = workshop.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
+#     
+#     # creating workshop
+#     new_workshop = Item.create!(
+#       user_id: User.all.sample.id,
+#       name: name,
+#       description: description.tr("\n", ''),
+#       price: price.tr('€.', ''),
+#       category: 'craft',
+#       capacity: rand(10),
+#       workshop: true
+#     )
+#     new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
+#     p "#{name} created"
+#     i += 1
+#   end
+#   p ''
+# end
 
-    # creating workshop
-    new_workshop = Item.create!(
-      user_id: User.all.sample.id,
-      name: name,
-      description: description.tr("\n", ''),
-      price: price.tr('€.', ''),
-      category: 'craft',
-      capacity: rand(10),
-      workshop: true
-    )
-    new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
-    p "#{name} created"
-    i += 1
-  end
-  p ''
-end
+# p 'Creating new products...'
 
-p 'Creating new products...'
+# p ''
 
-p ''
+# new_workshop = Item.create!(
+#   user_id: User.all.sample.id,
+#   name:
+#   description:
+#   price:
+#   category: 'craft',
+#   quantity: rand(10),
+#   workshop: false
+# )
 
-p 'All set! for now'
+# bags_url = 'https://www.etsy.com/shop/OriginGalleria?section_id=28357039'
+# bags = Nokogiri::HTML(open(bags_url).read)
+# p 'Looking for bags...'
+
+# clothes_url = 'https://www.etsy.com/shop/OriginGalleria?section_id=28862811'
+# clothes = Nokogiri::HTML(open(clothes_url).read)
+# p 'Looking for clothes...'
+
+# wall_hangings_url = 'https://www.etsy.com/shop/OriginGalleria?section_id=28341232'
+# wall_hangings = Nokogiri::HTML(open(wall_hangings_url).read)
+# p 'Looking for wall hangings...'
+
+# objects_url = 'https://www.etsy.com/shop/OriginGalleria?section_id=28352541'
+# objects = Nokogiri::HTML(open(objects_url).read)
+# p 'Looking for wall objects...'
+
+# [bags, clothes, wall_hangings].each do |product_type|
+#   i = 0
+#   product_type.search('.listing-link').each do |products|
+#     if i < 3
+#       i += 1
+#     else
+#       # scraping info
+#       product = Nokogiri::HTML(open(products.attribute('href').value).read)
+#       name = product.search('.wt-text-body-03.wt-line-height-tight.wt-break-word.wt-mb-xs-1').text.strip
+#       description = product.search('.wt-text-body-01.wt-break-word').text.strip
+#       price = product.search('.wt-text-title-03.wt-mr-xs-2').text.strip
+#       img = product.search('.wt-max-width-full.wt-horizontal-center.wt-vertical-center.carousel-image.wt-rounded')
+
+#       # creating products
+#       new_product = Item.create!(
+#         user_id: User.all.sample.id,
+#         name: name,
+#         description: description.tr("\n", ''),
+#         price: price.tr(' €.$BRL', ''),
+#         category: 'bag',
+#         capacity: rand(10),
+#         workshop: false
+#       )
+#       new_product.images.attach(io: open("#{img.attribute('src').value}"), filename: "workshop#{:id}.jpg")
+#       p "#{name} created"
+
+#     end
+#   end
+# end
