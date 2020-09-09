742ac4ee0538694b8639e8247cd5242934ff29a4
Cassia Aveliz <cassia.avelizr@gmail.com>
2020-09-09 12:53:34 -0300

    fix bug on workshops sedding and clean the code

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ Item.destroy_all
 p 'Destroying all users...'
 User.destroy_all
 
-p '--------------------'
+p ''
 
-p 'Creating 05 new Users...'
+p 'Creating 10 new Users...'
 
-5.times do
+10.times do
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
@@ -29,110 +29,57 @@ p 'Creating 05 new Users...'
   p "#{user.first_name} created with email #{user.email}"
 end
 
-p '--------------------'
+p ''
 
 p 'Creating new worskshops...'
 
-p 'Craft worskshops:'
+craft_url = 'https://www.viator.com/en-CA/Marrakech-tours/Craft-Classes/d5408-g26051-c33939'
+craft = Nokogiri::HTML(open(craft_url).read)
+p 'Looking for craft worskshops...'
 
-url = 'https://www.viator.com/en-CA/Marrakech-tours/Craft-Classes/d5408-g26051-c33939'
-html = Nokogiri::HTML(open(url).read)
+cooking_url = 'https://www.viator.com/en-CA/Marrakech-tours/Cooking-Classes/d5408-g6-c19'
+cooking = Nokogiri::HTML(open(cooking_url).read)
+p 'Looking for cooking worskshops...'
 
-i = 0
+painting_url = 'https://www.viator.com/en-CA/India-tours/Painting-Classes/d723-g26051-c32009'
+painting = Nokogiri::HTML(open(painting_url).read)
+p 'Looking for painting worskshops...'
 
-until i > 10
-  # scraping info
-  name = html.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
-  description = html.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
-  price = html.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
-  img = html.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
-  img.attribute("data-src").value
+pottery_url = 'https://www.viator.com/en-CA/India-tours/Pottery-Classes/d723-g26051-c32012'
+pottery = Nokogiri::HTML(open(pottery_url).read)
+p 'Looking for pottery worskshops...'
 
-  # creating workshop
-  new_workshop = Item.create!(
-    user_id: User.all.sample.id,
-    name: name,
-    description: description.tr("\n", ''),
-    price: price.tr('€.', ''),
-    category: 'craft',
-    capacity: rand(10),
-    workshop: true
+p ''
+
+[craft, cooking, painting, pottery].each do |workshop|
+  i = 0
+  until i > 5
+    # scraping info
+    name = workshop.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
+    description = workshop.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
+    price = workshop.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
+    img = workshop.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
+    img.attribute('data-src').value
+
+    # creating workshop
+    new_workshop = Item.create!(
+      user_id: User.all.sample.id,
+      name: name,
+      description: description.tr("\n", ''),
+      price: price.tr('€.', ''),
+      category: 'craft',
+      capacity: rand(10),
+      workshop: true
     )
-  new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
-
-  p "#{name} created"
-
-  i += 1
+    new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
+    p "#{name} created"
+    i += 1
+  end
+  p ''
 end
 
-p 'Pottery worskshops:'
+p 'Creating new products...'
 
-url = 'https://www.viator.com/en-CA/Marrakech-tours/Pottery-Classes/d5408-g26051-c32012'
-html = Nokogiri::HTML(open(url).read)
-
-i = 0
-
-until i > 10 
-  # scraping info
-  name = html.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
-  description = html.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
-  price = html.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
-  img = html.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
-  img.attribute("data-src").value
- 
-  # creating workshop
-  new_workshop = Item.create!(
-    user_id: User.all.sample.id,
-    name: name,
-    description: description.tr("\n", ''),
-    price: price.tr('€.', ''),
-    category: 'craft',
-    capacity: rand(10),
-    workshop: true
-    )
-  new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
-
-  p "#{name} created"
-
-  i += 1
-end
-
-p 'Cooking worskshops:'
-
-url = 'https://www.viator.com/en-CA/Marrakech-tours/Cooking-Classes/d5408-g6-c19'
-html = Nokogiri::HTML(open(url).read)
-
-i = 0
-
-until i > 10 
-  # scraping info
-  name = html.search("#productName#{i} .text-dark.highlight-able.card-link").text.strip
-  description = html.search("#productName#{i} .text-body.summary-text.mb-0").text.strip
-  price = html.search("#productName#{i} .h3.line-height-same.mb-0.price-font.text-md-right")[0].text.strip.tr('€.', '')
-  img = html.search("#productName#{i} .product-image.with-fallback.tb-b-to-g700-linear.scale-container.half-ease-in-out")
-  img.attribute("data-src").value
- 
-  # creating workshop
-  new_workshop = Item.create!(
-    user_id: User.all.sample.id,
-    name: name,
-    description: description.tr("\n", ''),
-    price: price.tr('€.', ''),
-    category: 'craft',
-    capacity: rand(10),
-    workshop: true
-    )
-  new_workshop.images.attach(io: open("#{img.attribute("data-src").value}"), filename: "workshop#{:id}.jpg")
-
-  p "#{name} created"
-
-  i += 1
-end
-
-# p '--------------------'
-
-# # p 'Creating new products...'
-
-p '--------------------'
+p ''
 
 p 'All set! for now'
