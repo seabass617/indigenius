4fab31d1eb740e7e26f9ffe9de734223d44ce942
Cassia Aveliz <cassia.avelizr@gmail.com>
2020-09-11 07:30:30 -0300

    Items divided into workshop and products

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.where(workshop: params[:format])
   end
 
   # GET /items/1
