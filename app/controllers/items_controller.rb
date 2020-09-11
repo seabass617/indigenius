6583f7cc5918ada64264d193be95fa7d1441efba
Cassia Aveliz <cassia.avelizr@gmail.com>
2020-09-11 08:04:11 -0300

    serveral small random

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,14 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: %i[show edit update destroy]
 
   # GET /items
   # GET /items.json
   def index
-    @items = Item.where(workshop: params[:format])
+    if params[:format].nil?
+      @items = Item.all
+    else
+      @items = Item.where(workshop: params[:format])
+    end
   end
 
   # GET /items/1
@@ -14,7 +18,9 @@ class ItemsController < ApplicationController
   end
 
   def listings
-    @items = Item.where("user_id = ?", current_user.id )
+    @items = Item.where('user_id = ?', current_user.id)
+    @workshops = @items.where(workshop: true)
+    @products = @items.where(workshop: false)
   end
 
   # GET /items/new
--- app/controllers/workshop_dates_controller.rb
+++ app/controllers/workshop_dates_controller.rb
@@ -1,59 +1,55 @@
 class WorkshopDatesController < ApplicationController
-  before_action :workshop_dates, only: [:new, :destroy]
+  before_action :workshop_dates, only: %i[new destroy]
 
-  def index 
+  def index
     @workshop_dates = WorkshopDate.all
-  end 
+  end
 
   def new
     @item = Item.find(params[:item_id])
     @workshop_date = WorkshopDate.new
-    @workshop_date.item = @item 
-    
+    @workshop_date.item = @item
   end
-  
+
   def create
     @item = Item.find(params[:item_id])
     @workshop_date = WorkshopDate.new(workshop_date_params)
     @workshop_date.item = @item
-    
+
     if @workshop_date.save
       redirect_to new_item_workshop_date_path(@item), notice: 'Your dates were successfully created'
     else
-      render :new, notice: "workshop_date not saved!"
-     
-     end 
+      render :new, notice: 'workshop_date not saved!'
+    end
   end
 
   def update
     @workshop_date = WorkshopDate.find(params[:id])
     @workshop_date.update(workshop_date_params)
-    #authorize @workshop_date
+    # authorize @workshop_date
     # if @workshop_date.save!
     #   notice: "workshop_date updated!"
     #  #redirect_to workshop_date_path(@workshop_date), notice: 'Your workshop_date was successfully updated.'
     # else
     #   notice: "workshop_date not updated"
     #  # render :new
-    # end 
-
+    # end
   end
 
   def destroy
     @item = Item.find(params[:item_id])
     @workshop_date = WorkshopDate.find(params[:id])
-    @workshop_date.destroy 
+    @workshop_date.destroy
     render :new
-    
   end
 
   private
+
   def workshop_date_params
-      params.require(:workshop_date).permit(:start_date, :end_date)
-  end  
+    params.require(:workshop_date).permit(:start_date, :end_date)
+  end
 
   def workshop_dates
    @workshop_dates = WorkshopDate.where(item_id: params[:item_id])
   end
-
 end
