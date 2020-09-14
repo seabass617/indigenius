1bbb8f47aad8d7538380714cfaee13ccaca6f7de
Saraah Bikai <supaerv@gmail.com>
2020-09-14 14:45:11 -0100

    Added Markers to Map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    
+     @markers = Item.geocoded.map do |item|
+      {
+        lat: item.latitude,
+        lng: item.longitude
+      }
+    end
   end
 end
